a69e3f3b05ec047b6dcccae4c1b78066c7173458
yonis65 <67803129+yonis65@users.noreply.github.com>
2023-08-03 22:09:57 +0200

    Update efbet.py

--- efbet.py
+++ efbet.py
@@ -1,265 +1,228 @@
-import datetime
-import requests
-from undetected_chromedriver import Chrome
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+import dearpygui.dearpygui as dpg
+import pandas as pd
+from efbet import EfBet
+from bigstake import BigStake
+import json
+
 import logging
+import logging.handlers
+
+VERSION = "V0.4"
 
 logger = logging.getLogger("mainlog")
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+logger.setLevel(logging.DEBUG)
 
-def fix_date(date):
-    if len(str(date)) == 1:
-        return "0"+ str(date)
-    return str(date)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(formatter)
 
-class EfBet():
-    def __init__(self, username, password) -> None:
-        self.s = requests.Session()
-        self.username = username
-        self.password = password
+logFilePath = "logs/app.log"
+file_handler = logging.handlers.TimedRotatingFileHandler(filename=logFilePath, when='midnight', backupCount=30)
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
 
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
-        self.init_browser()
-        logger.debug("[EfBet] Init session")
+class DataValues():
+    excel_path = None
+    __doc__ = "__doc__"
+
+def save_values():
+    json_data = {
+        "excel_path": DataValues.excel_path,
+        "cassa": DataValues.cassa,
+        "eventi": DataValues.eventi,
+        "prese": DataValues.prese,
+        "quota": DataValues.quota,
+        "bigstake_username": DataValues.bigstake_username,
+        "bigstake_password": DataValues.bigstake_password,
+        "efbet_username": DataValues.efbet_username,
+        "efbet_password": DataValues.efbet_password,
+    }
+
+    json.dump(json_data, open("settings.json", "w", encoding="utf-8"))
+    logger.debug("Setting saved")
+
+def load_values():
+    try:
+        json_data = json.load(open("settings.json", "r", encoding="utf-8"))
+    except:
+        return
     
-    def init_browser(self):
-        options = Options()
-        options.page_load_strategy = 'eager'
-        self.browser = Chrome(options=options)
-        self.browser.maximize_window()
+    dpg.set_value("Masianello Cassa", json_data["cassa"])
+    dpg.set_value("Masianello Eventi", json_data["eventi"])
+    dpg.set_value("Masianello Prese", json_data["prese"])
+    dpg.set_value("Masianello Quota", json_data["quota"])
 
-    def save_html_log(self):
-        with open(f"logs/html_{datetime.datetime.now().strftime('%d%m%Y_%H%M%S_%f')}.log", "w", encoding="utf-8") as f:
-            f.write(self.browser.page_source)
+    dpg.set_value("BigStake Username", json_data["bigstake_username"])
+    dpg.set_value("BigStake Password", json_data["bigstake_password"])
 
-    def login(self, n=0):
-        if n == 2:
-            logger.error("[EfBet] Can't login")
-            return False
+    dpg.set_value("Efbet Username", json_data["efbet_username"])
+    dpg.set_value("Efbet Password", json_data["efbet_password"])
 
-        self.browser.get("https://www.efbet.it/scommesse")
-        
-        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, "cg-username"))).send_keys(self.username)
-        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, "cg-password"))).send_keys(self.password)
-        self.browser.execute_script("cg_login()")
-        try:
-            WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, "cg-profile-popup-toggle")))
-        except:
-            logger.debug("[EfBet] Failed to login, retry")
-            return self.login(n+1)
-
-        logger.info("[EfBet] Logged in")
-        return True
-        
     
-    def search_match(self, match):
-        logger.debug(f"[EfBet] Searching for match {match}")
 
-        if not self.login():
-            return False
+def read_excel():
+    df = pd.read_excel(DataValues.excel_path)
 
-        time.sleep(2)
-        try:
-            el = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, "match-search-input")))
-            self.browser.execute_script("document.getElementById(\"match-search-input\").readOnly = false")
+    matches = []
+    for _, row in df.iterrows():
+        matches.append({"name": row["Partita"], "segno": str(row["Segno"])})
+    return matches
 
-            try:
-                el.click()
-            except:
-                self.browser.get("https://www.efbet.it/scommesse")
-            time.sleep(2)
-            el.send_keys(match)
-            time.sleep(4)
-            results = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, "autocomplete-search-results-id-1"))).find_elements(By.CLASS_NAME, "pointer")
-            if len(results) != 1:
-                logger.error(f"[EfBet] Match not found")
-                return False
+def read_data():
+    DataValues.cassa = dpg.get_value("Masianello Cassa")
+    DataValues.eventi = dpg.get_value("Masianello Eventi")
+    DataValues.prese = dpg.get_value("Masianello Prese")
+    DataValues.quota = dpg.get_value("Masianello Quota")
+
+    DataValues.bigstake_username = dpg.get_value("BigStake Username")
+    DataValues.bigstake_password = dpg.get_value("BigStake Password")
+
+    DataValues.efbet_username = dpg.get_value("Efbet Username")
+    DataValues.efbet_password = dpg.get_value("Efbet Password")
+
+def start():
+    logger.info("Bot started")
+    read_data()
+    missing_settings = [attr for attr, value in DataValues.__dict__.items() if not value]
+    if missing_settings:
+        logger.error("Missing Values:", ", ".join(missing_settings))
+        return
+
+    save_values()
+
+    bs = BigStake(DataValues.bigstake_username, DataValues.bigstake_password)
+    eb = EfBet(DataValues.efbet_username, DataValues.efbet_password)
+
+    bs.create_masaniello(DataValues.cassa, DataValues.eventi, DataValues.quota, DataValues.prese)
+
+    matches = read_excel()
+
+    n_match = 0
+    masaniello_working = True
+
+    while masaniello_working:
+        match = matches[n_match]
+        n_match += 1
+
+        quotes = eb.search_match(match["name"])
+
+        if quotes == False:
+            logger.info("Going to the next match")
+            continue
+
+        amount = bs.update_masaniello_quote(quotes[match["segno"]]["quota"])
+        if amount == 0:
+            masaniello_working = False
+            continue
+        
+        if eb.place_bet(quotes, match["segno"], amount, bs) == False:
+            logger.info("Going to the next match")
+            bs.go_previous()
+            continue
+
+        bet_result = eb.wait_bet_to_end()
+        ended, cassa = bs.update_masaniello_result(bet_result)
+        
+        masaniello_working = not ended
+        if float(cassa) <= 0:
+            masaniello_working = False
+            continue
+        
+
+    bs.delete_masaniello()
+    logger.info(f"Finished betting, cassa: {cassa}, matches: {len(bs.bets)}")
+
+
+def file_selected_callback(sender, app_data):
+    dpg.set_value("Excel Input File", app_data["file_path_name"])
+    DataValues.excel_path = app_data["file_path_name"]
+
+dpg.create_context()
+
+with dpg.file_dialog(show=False, callback=file_selected_callback, tag="Excel File Dialog", width=600 ,height=400):
+    dpg.add_file_extension("Excel files (*.xlsx){.xlsx}", color=(0, 255, 255, 255))
+
+with dpg.window(tag="Main Window"):
+    with dpg.tab_bar(tag="Tab Bar"):
+        with dpg.tab(label="Masaniello"):
+            with dpg.table(header_row=False):
+                dpg.add_table_column()
+                dpg.add_table_column()
+                
+                with dpg.table_row():
+                    dpg.add_text("Cassa: ")
+                    dpg.add_input_int(tag="Masianello Cassa")
             
-            results[0].click()
-            WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "blocco-quote-espanse")))
-            logger.info(f"[EfBet] Match found")
-            return self.scrape_quotes()
-        except Exception as e:
-            logger.error(f"[EfBet] Couldn't search the match")
-            logger.debug(f"[EfBet] Error: {e}")
-            return False
-    
-    def scrape_quote_type(self, names, selector):
-        quotes_dict = {}
-        try:
-            el = WebDriverWait(self.browser, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
-            quotes = el.find_elements(By.TAG_NAME, "div")
+                with dpg.table_row():
+                    dpg.add_text("Eventi: ")
+                    dpg.add_input_int(tag="Masianello Eventi")
+                
+                with dpg.table_row():
+                    dpg.add_text("Prese richieste: ")
+                    dpg.add_input_int(tag="Masianello Prese")
+                
+                with dpg.table_row():
+                    dpg.add_text("Quota media: ")
+                    dpg.add_input_double(tag="Masianello Quota")
 
-            for n, quote in enumerate(quotes):
-                el = quote.find_elements(By.TAG_NAME, "span")[1]
-                quotes_dict[names[n]] = {"element": el, "quota": el.text}
-        
-            return quotes_dict
-        except:
-            logger.debug(f"[EfBet] Can't find {', '.join(names)}")
-            return {}
-
-    def scrape_quotes(self):
-        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, "secondo-blocco-sport")))
-        self.browser.execute_script('document.querySelector("#secondo-blocco-sport div[data-id=m--1]").click()')
-
-        quotes_dict = {}
-        quotes_dict.update(self.scrape_quote_type(["1", "X", "2"], 'div[id$="_3_0"]'))
-        quotes_dict.update(self.scrape_quote_type(["U 0.5", "O 0.5"], 'div[id$="_7989_50"]'))
-
-        quotes_dict.update(self.scrape_quote_type(["U 1.5", "O 1.5"], 'div[id$="_7989_150"]'))
-
-        quotes_dict.update(self.scrape_quote_type(["U 2.5", "O 2.5"], 'div[id$="_7989_250"]'))
-
-        quotes_dict.update(self.scrape_quote_type(["1 1T", "X 1T", "2 1T"], 'div[id$="_14_0"]'))
-
-        quotes_dict.update(self.scrape_quote_type(["U 0.5 1T", "O 0.5 1T"], 'div[id$="9942_65541"]'))
-
-
-        quote_str = []
-        for name, value in quotes_dict.items():
-            quote_str.append(f"{name}: {value['quota']}")
-
-        quote_str = ", ".join(quote_str)
-
-        logger.debug(f"[EfBet] Scraped quotes {quote_str}")
-
-        return quotes_dict
-    
-    def close_browser(self):
-        self.browser.quit()
-
-    def place_bet(self, quotes, bet, amount, bs):
-        try:
-            logger.info(f"[EfBet] Placing bet quota: {quotes[bet]['quota']}, bet: {bet}, amount: {amount}")
-            quotes[bet]["element"].click()
-
-            el = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[id^=input-calc-for-bet-event-coupon-]")))
-            el.clear()
-            el.send_keys(str(amount))
-
-            WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "bottone.bg-verde.bianco.maiuscolo"))).click()
-            try:
-                WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "bottone.bg-blu.bianco.maiuscolo"))).click()
-            except:
-
-                #QUOTA VARIATA
-                try:
-                    el = WebDriverWait(self.browser, 3).until(EC.visibility_of_element_located((By.CLASS_NAME, "bottoni-quota-variata")))
-                    time.sleep(2)
-                    el.find_element(By.CLASS_NAME, "bottone.bg-verde.bianco.maiuscolo").click()
-                    el = self.browser.find_element(By.CLASS_NAME, "scommessa__esito")
-                    quotes[bet]["quota"] = el.find_elements(By.CLASS_NAME, "scommessa__esito__valoreQuota")[1].text
-                    bs.go_previous()
-                    bs.update_masaniello_quote(quotes[bet]["quota"])
-                    logger.debug(f"[EfBet] Quote changed to {quotes[bet]['quota']}")
+        with dpg.tab(label="Credenziali"):
+            dpg.add_text("BigStake")
+            with dpg.child_window(label="BigStake", height=63):
+                with dpg.table(header_row=False):
+                    dpg.add_table_column()
+                    dpg.add_table_column()
                     
-                except Exception as e:
-                    self.save_html_log()
-                    logger.error(f"[EfBet] Can't place the bet")
-                    logger.debug(f"[EfBet] Error: {e}")
-                    return False
+                    with dpg.table_row():
+                        dpg.add_text("Username: ")
+                        dpg.add_input_text(tag="BigStake Username")
+                        
+                    with dpg.table_row():
+                        dpg.add_text("Password: ")
+                        dpg.add_input_text(tag="BigStake Password")
+            
+            dpg.add_text("Efbet")
+            with dpg.child_window(label="Efbet", height=63):
+                with dpg.table(header_row=False):
+                    dpg.add_table_column()
+                    dpg.add_table_column()
+                    
+                    with dpg.table_row():
+                        dpg.add_text("Username: ")
+                        dpg.add_input_text(tag="Efbet Username")
 
-            if not self.check_bet():
-                self.save_html_log()
-                logger.error("[EfBet] The bet didn't get placed")
-                return False
+                        
+                    with dpg.table_row():
+                        dpg.add_text("Password: ")
+                        dpg.add_input_text(tag="Efbet Password")
 
-            return quotes[bet]["quota"]
-        except Exception as e:
-            logger.error(f"[EfBet] Couldn't place the bet")
-            logger.debug(f"[EfBet] Error: {e}")
-            return False
+        with dpg.tab(label="File"):
+            with dpg.group(horizontal=True):
+                dpg.add_text("Choose excel file: ")
+                dpg.add_input_text(tag="Excel Input File")
+                dpg.add_button(label="Directory Selector", callback=lambda: dpg.show_item("Excel File Dialog"))
 
+    dpg.add_button(tag="Start Button", label="Start", callback=start)
 
-    def get_bets(self):
-        now = datetime.datetime.now()
-        before = now - datetime.timedelta(days = 7)
-        dateFrom = fix_date(before.year) + fix_date(before.month) + fix_date(before.day)
-        dateTo = fix_date(now.year) + fix_date(now.month) + fix_date(now.day)
+def resize():
+    x = (dpg.get_viewport_width() / 2) - ( dpg.get_item_rect_size("Start Button")[0] / 2)
+    y = dpg.get_viewport_height() - dpg.get_item_rect_size("Start Button")[1] - 300
+    dpg.set_item_pos("Start Button", (x, y))
 
-        payload = {
-            "systemCode": "EFBET",
-            "lingua": "IT",
-            "hash": "",
-            "token": self.get_token(),
-            "accountIds": "11665;0",
-            "dateFrom": dateFrom,
-            "dateTo": dateTo
-        }
+with dpg.item_handler_registry(tag="window_handler"):
+    dpg.add_item_resize_handler(callback=resize)
 
-        r = requests.post("https://www.efbet.it/XSportDatastore/getMyBets", data=payload)
-        return r.json()
+logger.info(f'App started ({VERSION})')
 
-    def check_bet(self):
-        if len(self.get_bets()["opns"]) == 0:
-            return False
-        return True
+dpg.bind_item_handler_registry("Main Window", "window_handler")
+dpg.set_frame_callback(1, load_values)
 
-    def get_token(self):
-        payload = {
-            "username": "npsassari",
-            "password": "Forzainter1$",
-            "systemCode": "EFBET",
-            "language": "it",
-            "migliaia": ".",
-            "decimali": ",",
-            "timezoneOffset": 0,
-            "hash": ""
-        }
-        r = requests.post("https://www.efbet.it/loginContogioco", data=payload)
-        token = r.json()["sessionToken"]
-        logger.debug(f"[EfBet] Session token {token}")
-        return token
-
-    def wait_bet_to_end(self):
-        logger.info(f"[EfBet] Waiting for the bet to end")
-        time.sleep(90 * 60)
-
-        MINUTES = 5
-
-        while True:
-            data = self.get_bets()
-            opens = data["opns"]
-            if len(opens) == 0:
-                res = 1
-                if data["ends"][0]["st"] == "losing":
-                    res = 0
-
-                logger.info(f"[EfBet] Bet finished with result {res}, {data['ends'][0]['st']}")
-                return res
-
-            logger.debug(f"[EfBet] Bet checked, not finished yet")
-            time.sleep(MINUTES * 60)
-        
-if __name__ == "__main__":
-    import logging.handlers
-
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-    logger.setLevel(logging.DEBUG)
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(formatter)
-
-    logFilePath = "logs/app.log"
-    file_handler = logging.handlers.TimedRotatingFileHandler(filename=logFilePath, when='midnight', backupCount=30)
-    file_handler.setFormatter(formatter)
-    file_handler.setLevel(logging.DEBUG)
-
-    logger.addHandler(file_handler)
-    logger.addHandler(stream_handler)
-
-    eb = EfBet("npsassari", "Forzainter1$")
-
-    quotes = eb.search_match("14460")
-    print(quotes)
-    input()
-
-    # eb.place_bet(quotes, "1", 1)
-
-    # bet_result = eb.wait_bet_to_end()
-    # print(bet_result)
+dpg.create_viewport(title=f'Efbet Auto Bet ({VERSION})', width=900, height=600)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window("Main Window", True)
+dpg.start_dearpygui()
+dpg.destroy_context()
