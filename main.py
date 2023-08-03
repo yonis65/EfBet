1c1faabb54d343f95ba16f7e91e7b3e8749b2b0a
yonis65 <67803129+yonis65@users.noreply.github.com>
2023-08-03 13:46:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 import logging
 import logging.handlers
 
-VERSION = "V0.1"
+VERSION = "V0.2"
 
 logger = logging.getLogger("mainlog")
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
